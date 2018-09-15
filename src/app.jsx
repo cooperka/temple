0813ec63e96046611085373cbcfaa10faf83fae7
Kevin Cooper <cooperka92@gmail.com>
2018-09-15 18:44:55 -0400

    :shirt: Switch to eslint-config-cooperka

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default class App extends React.Component {
+
   render() {
     return (
       <div>
@@ -8,4 +9,5 @@ export default class App extends React.Component {
       </div>
     );
   }
+
 }
