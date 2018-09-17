1bbb7bde449a877e90044d644ebc8f782e19634e
Kevin Cooper <cooperka92@gmail.com>
2018-09-16 21:41:47 -0400

    :white_check_mark: Add jest + tests

---
+++ src/components/App/component.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Component from './component';
+
+it('renders as expected', () => {
+  const component = renderer.create(
+    <Component />,
+  );
+  expect(component.toJSON()).toMatchSnapshot();
+});
