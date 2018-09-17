5f8fe34729d9fc1542d44e4c4b0236fc18972007
Kevin Cooper <cooperka92@gmail.com>
2018-09-16 22:27:16 -0400

    :sparkles: Add button to copy to clipboard

--- src/components/App/component.js
+++ src/components/App/component.js
@@ -1,11 +1,19 @@
+import { clipboard } from 'electron';
 import React, { Component } from 'react';
 
 export default class App extends Component {
 
+  copyText = () => {
+    clipboard.writeText('test');
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to React!</h2>
+        <h2>Temple</h2>
+        <button type="button" onClick={this.copyText}>
+          Copy stuff
+        </button>
       </div>
     );
   }
