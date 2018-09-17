b322c6986df9ba114920cb3fb176215ad2496fa5
Kevin Cooper <cooperka92@gmail.com>
2018-09-16 22:50:05 -0400

    :package: Modularize default index.js

--- src/index.js
+++ src/index.js
@@ -10,52 +10,49 @@ const isDevMode = process.execPath.match(/[\\/]electron/);
 
 if (isDevMode) enableLiveReload({ strategy: 'react-hmr' });
 
-const createWindow = async () => {
-  // Create the browser window.
+async function handleReady() {
+  await createWindow();
+}
+
+async function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
   });
 
-  // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Open the DevTools.
+  // Open the Chrome DevTools.
   if (isDevMode) {
     await installExtension(REACT_DEVELOPER_TOOLS);
     mainWindow.webContents.openDevTools();
   }
 
-  // Emitted when the window is closed.
   mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
+    // Dereference the window object; usually you would store windows
+    // in an array if your app supports multi windows.
     mainWindow = null;
   });
-};
+}
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
+function handleAllClosed() {
   // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+  // to stay active until the user quits explicitly with Cmd + Q.
   if (process.platform !== 'darwin') {
     app.quit();
   }
-});
+}
 
-app.on('activate', () => {
+function handleActivate() {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
-});
+}
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+app.on('ready', handleReady);
+
+app.on('window-all-closed', handleAllClosed);
+
+app.on('activate', handleActivate);
