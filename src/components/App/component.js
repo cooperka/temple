18e23e497d32ad17db4a0559ed66bcbd7cea471a
Kevin Cooper <cooperka92@gmail.com>
2018-10-22 17:07:43 -0400

    :package: Add boilerplate withStyles

--- src/components/App/component.js
+++ src/components/App/component.js
@@ -1,6 +1,7 @@
+import * as R from 'ramda';
 import { clipboard } from 'electron';
 import React, { Component } from 'react';
-import { Typography, Button } from '@material-ui/core';
+import { withStyles, Typography, Button } from '@material-ui/core';
 
 class App extends Component {
 
@@ -23,4 +24,9 @@ class App extends Component {
 
 }
 
-export default App;
+const styles = {
+};
+
+export default R.compose(
+  withStyles(styles),
+)(App);
